2292a94e983fac621ec36d69775deeef5587b96a
Nathan-Niels <nsnielson@outlook.com>
2024-09-11 10:47:09 -0600

    Committing Bike Share lm

---
+++ BikeShareAnalysis.R
@@ -0,0 +1,31 @@
+library(tidymodels)
+library(tidyverse)
+library(vroom)
+
+train_data <- vroom("C:/Users/nsnie/OneDrive/BYU Classes/Fall 2024/STAT 348/KaggleBikeShare/train.csv")
+test_data <- vroom("C:/Users/nsnie/OneDrive/BYU Classes/Fall 2024/STAT 348/KaggleBikeShare/test.csv")
+
+## Setup and Fit Linear Regression Model
+my_linear_model <- linear_reg() %>% 
+  set_engine("lm") %>% 
+  set_mode("regression") %>% 
+  fit(formula = count ~ season+holiday+workingday+weather+temp+humidity+windspeed,
+      data = train_data)
+
+## Generate Predictions Using LInear Model
+bike_predictions <- predict(my_linear_model,
+                            new_data=test_data)
+bike_predictions
+
+## Format the Predictions for Kaggle Submission
+kaggle_submission <- bike_predictions %>% 
+  bind_cols(., test_data) %>% 
+  select(datetime, .pred) %>% 
+  rename(count = .pred) %>% 
+  mutate(count = pmax(0, count)) %>% 
+  mutate(datetime = as.character(format(datetime)))
+
+## Write out the file
+vroom_write(x = kaggle_submission,
+            file = "C:/Users/nsnie/OneDrive/BYU Classes/Fall 2024/STAT 348/KaggleBikeShare/LinearPreds.csv", 
+            delim = ",")
