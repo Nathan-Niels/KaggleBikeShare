c6a15d7165cd1d836daebe43bb876cce18d9861f
Nathan-Niels <nsnielson@outlook.com>
2024-09-17 08:03:00 -0600

    uploading changes to BikeShare.R

--- BikeShare.R
+++ BikeShare.R
@@ -5,7 +5,7 @@ library(skimr)
 library(DataExplorer)
 library(patchwork)
 
-
+## EDA
 train_data <- vroom("C:/Users/nsnie/OneDrive/BYU Classes/Fall 2024/STAT 348/KaggleBikeShare/train.csv")
 train_data
 
@@ -51,3 +51,50 @@ plot4
 
 # 4 panel ggplot of 4 key features
 (plot1 + plot2) / (plot3 + plot4)
+
+## Data Cleaning
+test_data <- vroom("C:/Users/nsnie/OneDrive/BYU Classes/Fall 2024/STAT 348/KaggleBikeShare/test.csv")
+
+clean_train_data <- train_data %>% 
+  mutate(log_count = log(count)) %>% 
+  select(-casual, -registered, -count)
+view(clean_train_data)
+clean_train_data
+
+## Feature Engineering
+bike_recipe <- recipe(log_count ~ ., data = clean_train_data) %>% 
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) %>% 
+  step_mutate(weather = as.factor(weather)) %>% 
+  step_time(datetime, features = "hour") %>% 
+  step_mutate(datetime = as.factor(datetime_hour)) %>% 
+  step_mutate(season = as.factor(season)) %>%
+  step_mutate(holiday = as.factor(holiday)) %>% 
+  step_mutate(workingday = as.factor(workingday)) %>% 
+  step_rm(datetime_hour) %>% 
+  step_rm(atemp)
+
+prepped_bike_recipe <- prep(bike_recipe)
+bake(prepped_bike_recipe, new_data = clean_train_data)
+
+bike_lm <- linear_reg() %>% 
+  set_engine("lm") %>% 
+  set_mode("regression")
+
+bike_workflow <- workflow() %>% 
+  add_recipe(bike_recipe) %>% 
+  add_model(bike_lm) %>% 
+  fit(data = clean_train_data)
+
+bike_preds <- exp(predict(bike_workflow, new_data = test_data))
+
+# Prepare for submission
+kaggle_submission <- bike_preds %>% 
+  bind_cols(., test_data) %>% 
+  select(datetime, .pred) %>% 
+  rename(count = .pred) %>% 
+  mutate(count = pmax(0, count)) %>% 
+  mutate(datetime = as.character(format(datetime)))
+
+vroom_write(x = kaggle_submission,
+            file = "C:/Users/nsnie/OneDrive/BYU Classes/Fall 2024/STAT 348/KaggleBikeShare/LinearPreds.csv", 
+            delim = ",")
