1715492fb78bccd4c27940c93155628e1da5708b
Nathan-Niels <nsnielson@outlook.com>
2024-09-20 09:49:05 -0600

    added tuning parameter selection

--- BikeShare.R
+++ BikeShare.R
@@ -67,11 +67,11 @@ bike_recipe <- recipe(log_count ~ ., data = clean_train_data) %>%
   step_mutate(weather = ifelse(weather == 4, 3, weather)) %>% 
   step_mutate(weather = as.factor(weather)) %>% 
   step_time(datetime, features = "hour") %>% 
-  step_mutate(datetime = as.factor(datetime_hour)) %>% 
+  step_mutate(datetime_hour = as.factor(datetime_hour)) %>% 
   step_mutate(season = as.factor(season)) %>%
   step_mutate(holiday = as.factor(holiday)) %>% 
   step_mutate(workingday = as.factor(workingday)) %>% 
-  step_rm(datetime_hour) %>% 
+  step_rm(datetime) %>% 
   step_rm(atemp) %>% 
   step_dummy(all_nominal_predictors()) %>% 
   step_normalize(all_numeric_predictors()) 
@@ -123,4 +123,62 @@ kaggle_submission <- preg_bike_preds %>%
 
 vroom_write(x = kaggle_submission,
             file = "C:/Users/nsnie/OneDrive/BYU Classes/Fall 2024/STAT 348/KaggleBikeShare/PregLinearPreds.csv", 
+            delim = ",")
+
+## Tuning Model
+
+# Penalized linear regression model
+tpreg_model <- linear_reg(penalty = tune(),
+                          mixture = tune()) %>% # Setting these as tuning parameters
+  set_engine("glmnet")
+
+# Set workflow
+tpreg_wf <- workflow() %>% 
+  add_recipe(bike_recipe) %>% 
+  add_model(tpreg_model)
+
+# Grid of values to tune over
+tune_grid <- grid_regular(penalty(),
+                          mixture(),
+                          levels = 5) # 25 total tuning parameter possibilities
+
+# Split data for CV
+folds <- vfold_cv(clean_train_data, v = 5, repeats = 1)
+
+# Run CV
+CV_results <- tpreg_wf %>% 
+  tune_grid(resamples = folds,
+            grid = tune_grid,
+            metrics = metric_set(rmse, mae, rsq))
+
+# Plot Results
+collect_metrics(CV_results) %>% 
+  filter(.metric == "rmse") %>% 
+  ggplot(data = ., 
+         aes(x = penalty, y = mean, color = factor(mixture))) +
+  geom_line()
+
+# Find Best Tuning Parameters
+best_tune <- CV_results %>% 
+  select_best(metric = "rmse")
+best_tune
+
+# Finalize workflow and fit it
+final_wf <- tpreg_wf %>% 
+  finalize_workflow(best_tune) %>% 
+  fit(data = clean_train_data)
+
+# Predict
+tpreg_bike_preds <- exp(predict(final_wf, new_data = test_data))
+
+# Prepare for submission
+kaggle_submission <- tpreg_bike_preds %>% 
+  bind_cols(., test_data) %>% 
+  select(datetime, .pred) %>% 
+  rename(count = .pred) %>% 
+  mutate(count = pmax(0, count)) %>% 
+  mutate(datetime = as.character(format(datetime)))
+
+vroom_write(x = kaggle_submission,
+            file = "C:/Users/nsnie/OneDrive/BYU Classes/Fall 2024/STAT 348/KaggleBikeShare/TPregLinearPreds.csv", 
             delim = ",")
